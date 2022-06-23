518bee0732bd1c859d8fb818649570ab4562dc8b
Danny Froberg <danny@froberg.org>
2022-06-23 18:25:36 +0200

    (feat) add more

--- dot_config/private_fish/custom_functions.d/wakatime.fish
+++ dot_config/private_fish/custom_functions.d/wakatime.fish
@@ -12,17 +12,22 @@
 # ~/Sites/ on a Mac, but it's easy to modify
 
 set -l project
-
-if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
-elseif echo (pwd) | grep -qEi "^/home/$USER/projects/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
-elseif echo (pwd) | grep -qEi "^/home/$USER/cluster/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
-elseif echo (pwd) | grep -qEi "^/home/$USER/infura/"
-    set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+set -l project
+set -l gitproject (test (which git); git rev-parse --show-toplevel 2>&1)
+if test -z "$gitproject"
+    set  project (echo $gitproject | sed "s#^/home/$USER/\\([^/]*\\).*#\\1#")
 else
-    set  project "Terminal"
+    if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
+        set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+    else if echo (pwd) | grep -qEi "^/home/$USER/projects/"
+        set  project (echo (pwd) | sed "s#^/home/$USER/projects/\\([^/]*\\).*#\\1#")
+    else if echo (pwd) | grep -qEi "^/home/$USER/cluster/"
+        set  project (echo (pwd) | sed "s#^/home/$USER/cluster/\\([^/]*\\).*#\\1#")
+    else if echo (pwd) | grep -qEi "^/home/$USER/infura/*"
+        set  project (echo (pwd) | sed "s#^/home/$USER/infura/\\([^/]*\\).*#\\1#")
+    else
+        set  project "Terminal"
+    end
 end
 
 wakatime-cli --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
