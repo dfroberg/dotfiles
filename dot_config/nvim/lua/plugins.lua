260ddb8a660b28f2084c6982b52fc26eb047fce3
Danny Froberg <danny@froberg.org>
2022-07-06 09:28:34 +0200

    (fix) Test solution for odd path issue

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,10 +1,13 @@
 local fn = vim.fn
 local cmd = vim.cmd
+vim.o.runtimepath = vim.fn.stdpath('data') .. '/site/pack/*/start/*,' .. vim.o.runtimepath
 
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   _G.packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
+
+vim.cmd [[packadd packer.nvim]] -- packadd packer module
 
 require('packer').startup({
   function(use)
