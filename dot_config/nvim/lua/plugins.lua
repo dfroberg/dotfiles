eb7d61c0ba0d82b7c8940b45480fc4cc82440452
Danny Froberg <danny@froberg.org>
2022-10-26 11:26:29 +0200

    fix: wip

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -23,6 +23,8 @@ packer.init({
 packer.startup({
   function(use)
     use 'wbthomason/packer.nvim'
+    use { "williamboman/mason.nvim" }
+    use { "williamboman/mason-lspconfig.nvim" }
 
     -- Syntax highlighting
     use 'pearofducks/ansible-vim'
