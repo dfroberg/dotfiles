8bfef7ce747343b8b33398fc50cda8e3682024f2
Danny Froberg <danny@froberg.org>
2024-02-27 09:51:31 +0100

    chore: test

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -26,7 +26,7 @@ require('lazy').setup({
     'pearofducks/ansible-vim',
     'alker0/chezmoi.vim',
     { 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end },
-    'folke/tokyonight.nvim',
+    {'folke/tokyonight.nvim', config = function() require('tokyonight').setup().style = 'night' end },
 
     -- Additional plugins from your selection
     'stevearc/dressing.nvim',
