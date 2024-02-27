2b15d12570f7d27abfac5941791dbe71589dc4c3
Danny Froberg <danny@froberg.org>
2024-02-27 09:29:34 +0100

    fix: play with lazy

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,214 +1,244 @@
 local fn = vim.fn
 local cmd = vim.cmd
 
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/lazy/lazy.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  _G.packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+   fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    install_path,
+  })
 end
+vim.opt.rtp:prepend(install_path)
 
-local packer = require('packer')
+cmd 'packadd lazy.nvim'
 
-packer.init({
-  display = {
-    open_fn = function()
-      return require('packer.util').float({ border = 'single' })
-    end
+vim.g.mapleader = " "
+
+require('lazy').setup({
+  plugins = {
+    -- Your plugins go here. For example:
+    { "williamboman/mason.nvim", config = function() require("mason").setup() end },
+    { "williamboman/mason-lspconfig.nvim", config = function() require("mason-lspconfig").setup() end },
+    'pearofducks/ansible-vim',
+    'alker0/chezmoi.vim',
+    { 'norcalli/nvim-colorizer.lua', config = function() require('colorizer').setup() end },
+    'folke/tokyonight.nvim',
+    -- Add more plugins as needed
   },
-  profile = {
-    enable = true,
-    threshold = 1,
-  }
 })
 
-packer.startup({
-  function(use)
-    use 'wbthomason/packer.nvim'
-    use { "williamboman/mason.nvim" 
-        , config = function()
-            require("mason").setup()
-          end
-        }
-    use { "williamboman/mason-lspconfig.nvim" 
-        , config = function()
-            require("mason-lspconfig").setup()
-          end
-        }
+-- local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+-- if fn.empty(fn.glob(install_path)) > 0 then
+--   _G.packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+-- end
 
-    -- Syntax highlighting
-    use 'pearofducks/ansible-vim'
-    use 'alker0/chezmoi.vim'
-    use {
-      'norcalli/nvim-colorizer.lua',
-      config = function()
-        require('colorizer').setup()
-      end
-    }
+-- local packer = require('packer')
 
-    -- Appearance
-    use 'folke/tokyonight.nvim'
-    use 'stevearc/dressing.nvim'
-    use {
-      'nvim-lualine/lualine.nvim',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = function()
-        require('configs._lualine')
-      end
-    }
-    use {
-      'lukas-reineke/indent-blankline.nvim',
-      config = function()
-        require('indent_blankline').setup()
-      end
-    }
+-- packer.init({
+--   display = {
+--     open_fn = function()
+--       return require('packer.util').float({ border = 'single' })
+--     end
+--   },
+--   profile = {
+--     enable = true,
+--     threshold = 1,
+--   }
+-- })
 
-    -- LSP
-    use {
-      'neovim/nvim-lspconfig',
-      requires = {
-        'b0o/schemastore.nvim',
-        'ray-x/lsp_signature.nvim',
-        'lewis6991/gitsigns.nvim',
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig',
-        'jose-elias-alvarez/null-ls.nvim',
-        'nvim-lua/plenary.nvim',
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-      },
-      config = function()
-        require('configs._lspconfig')
-      end
-    }
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      config = function()
-        require('configs._treesitter')
-      end
-    }
+-- packer.startup({
+--   function(use)
+--     use 'wbthomason/packer.nvim'
+--     use { "williamboman/mason.nvim" 
+--         , config = function()
+--             require("mason").setup()
+--           end
+--         }
+--     use { "williamboman/mason-lspconfig.nvim" 
+--         , config = function()
+--             require("mason-lspconfig").setup()
+--           end
+--         }
 
-    -- Autocompletion
-    use {
-      'hrsh7th/nvim-cmp',
-      config = function()
-        require('configs._nvim-cmp')
-      end
-    }
-    -- nvim-cmp dependencies but can't be listed as requires
-    -- because of lazyloading
-    use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
-    use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }
-    use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }
-    use { 'f3fora/cmp-spell', after = 'nvim-cmp' }
-    use { 'saadparwaiz1/cmp_luasnip', after = { 'nvim-cmp', 'LuaSnip' }}
-    use { 'onsails/lspkind-nvim', module = 'lspkind' }
-    use {
-      'L3MON4D3/LuaSnip',
-      requires = {
-        'rafamadriz/friendly-snippets'
-      },
-      module = 'luasnip',
-    }
+--     -- Syntax highlighting
+--     use 'pearofducks/ansible-vim'
+--     use 'alker0/chezmoi.vim'
+--     use {
+--       'norcalli/nvim-colorizer.lua',
+--       config = function()
+--         require('colorizer').setup()
+--       end
+--     }
 
-    use {
-      'folke/trouble.nvim',
-      opt = true,
-      event = 'BufReadPost',
-      config = function()
-        require('trouble').setup()
-      end
-    }
-    use {
-      'windwp/nvim-ts-autotag',
-      config = function()
-        require('nvim-ts-autotag').setup()
-      end
-    }
+--     -- Appearance
+--     use 'folke/tokyonight.nvim'
+--     use 'stevearc/dressing.nvim'
+--     use {
+--       'nvim-lualine/lualine.nvim',
+--       requires = { 'kyazdani42/nvim-web-devicons' },
+--       config = function()
+--         require('configs._lualine')
+--       end
+--     }
+--     use {
+--       'lukas-reineke/indent-blankline.nvim',
+--       config = function()
+--         require('indent_blankline').setup()
+--       end
+--     }
 
-    -- Fuzzy finder
-    use {
-      'ibhagwan/fzf-lua',
-      requires = { 'kyazdani42/nvim-web-devicons' },
-      config = function()
-        require('configs._fzf-lua')
-      end
-    }
-    use {
-      'phaazon/hop.nvim',
-      branch = 'v2',
-      config = function()
-        require('hop').setup()
-      end
-    }
+--     -- LSP
+--     use {
+--       'neovim/nvim-lspconfig',
+--       requires = {
+--         'b0o/schemastore.nvim',
+--         'ray-x/lsp_signature.nvim',
+--         'lewis6991/gitsigns.nvim',
+--         'williamboman/mason.nvim',
+--         'williamboman/mason-lspconfig',
+--         'jose-elias-alvarez/null-ls.nvim',
+--         'nvim-lua/plenary.nvim',
+--         'WhoIsSethDaniel/mason-tool-installer.nvim',
+--       },
+--       config = function()
+--         require('configs._lspconfig')
+--       end
+--     }
+--     use {
+--       'nvim-treesitter/nvim-treesitter',
+--       run = ':TSUpdate',
+--       config = function()
+--         require('configs._treesitter')
+--       end
+--     }
 
-    -- Enhancement
-    use {
-      'max397574/better-escape.nvim',
-      config = function()
-        require('better_escape').setup { mapping = { 'ii' }}
-      end
-    }
+--     -- Autocompletion
+--     use {
+--       'hrsh7th/nvim-cmp',
+--       config = function()
+--         require('configs._nvim-cmp')
+--       end
+--     }
+--     -- nvim-cmp dependencies but can't be listed as requires
+--     -- because of lazyloading
+--     use { 'hrsh7th/cmp-nvim-lsp', after = 'nvim-cmp' }
+--     use { 'hrsh7th/cmp-buffer', after = 'nvim-cmp' }
+--     use { 'hrsh7th/cmp-path', after = 'nvim-cmp' }
+--     use { 'f3fora/cmp-spell', after = 'nvim-cmp' }
+--     use { 'saadparwaiz1/cmp_luasnip', after = { 'nvim-cmp', 'LuaSnip' }}
+--     use { 'onsails/lspkind-nvim', module = 'lspkind' }
+--     use {
+--       'L3MON4D3/LuaSnip',
+--       requires = {
+--         'rafamadriz/friendly-snippets'
+--       },
+--       module = 'luasnip',
+--     }
 
-    -- System utility
-    use 'numToStr/FTerm.nvim'
-    use {
-      'tamago324/lir.nvim',
-      requires = {
-        'nvim-lua/plenary.nvim',
-        'tamago324/lir-git-status.nvim',
-        'kyazdani42/nvim-web-devicons',
-      },
-      config = function ()
-        require('configs._lir')
-      end
-    }
-    use {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-        require('configs._gitsigns')
-      end
-    }
+--     use {
+--       'folke/trouble.nvim',
+--       opt = true,
+--       event = 'BufReadPost',
+--       config = function()
+--         require('trouble').setup()
+--       end
+--     }
+--     use {
+--       'windwp/nvim-ts-autotag',
+--       config = function()
+--         require('nvim-ts-autotag').setup()
+--       end
+--     }
 
-    -- Editing support
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
-    }
-    use {
-      'windwp/nvim-autopairs',
-      config = function()
-        require('configs._autopairs')
-      end
-    }
-    use {
-      'kylechui/nvim-surround',
-      config = function()
-        require('nvim-surround').setup()
-      end
-    }
-    use 'taybart/b64.nvim'
+--     -- Fuzzy finder
+--     use {
+--       'ibhagwan/fzf-lua',
+--       requires = { 'kyazdani42/nvim-web-devicons' },
+--       config = function()
+--         require('configs._fzf-lua')
+--       end
+--     }
+--     use {
+--       'phaazon/hop.nvim',
+--       branch = 'v2',
+--       config = function()
+--         require('hop').setup()
+--       end
+--     }
 
-    -- Braindead doctor
-    use {
-      'folke/which-key.nvim',
-      config = function()
-        require('which-key').setup()
-      end
-    }
+--     -- Enhancement
+--     use {
+--       'max397574/better-escape.nvim',
+--       config = function()
+--         require('better_escape').setup { mapping = { 'ii' }}
+--       end
+--     }
 
-    -- Other
-    use 'andweeb/presence.nvim'
+--     -- System utility
+--     use 'numToStr/FTerm.nvim'
+--     use {
+--       'tamago324/lir.nvim',
+--       requires = {
+--         'nvim-lua/plenary.nvim',
+--         'tamago324/lir-git-status.nvim',
+--         'kyazdani42/nvim-web-devicons',
+--       },
+--       config = function ()
+--         require('configs._lir')
+--       end
+--     }
+--     use {
+--       'lewis6991/gitsigns.nvim',
+--       config = function()
+--         require('configs._gitsigns')
+--       end
+--     }
 
-    if packer_bootstrap then
-      require('packer').sync()
-    end
-  end
-})
+--     -- Editing support
+--     use {
+--       'numToStr/Comment.nvim',
+--       config = function()
+--         require('Comment').setup()
+--       end
+--     }
+--     use {
+--       'windwp/nvim-autopairs',
+--       config = function()
+--         require('configs._autopairs')
+--       end
+--     }
+--     use {
+--       'kylechui/nvim-surround',
+--       config = function()
+--         require('nvim-surround').setup()
+--       end
+--     }
+--     use 'taybart/b64.nvim'
 
-cmd [[
-  augroup PackerSync
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup END
-]]
+--     -- Braindead doctor
+--     use {
+--       'folke/which-key.nvim',
+--       config = function()
+--         require('which-key').setup()
+--       end
+--     }
+
+--     -- Other
+--     use 'andweeb/presence.nvim'
+
+--     if packer_bootstrap then
+--       require('packer').sync()
+--     end
+--   end
+-- })
+
+-- cmd [[
+--   augroup PackerSync
+--     autocmd!
+--     autocmd BufWritePost plugins.lua source <afile> | PackerSync
+--   augroup END
+-- ]]
